3fd6c7594f80355f2a06f791638a08c16ebb8b32
Marwan <72122152+marwanbasemP3@users.noreply.github.com>
2024-11-12 03:36:30 +0200

    initial implementation

---
+++ mzip/src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+struct data { uint32_t size; char chr; };
+
+struct data* compress_data(char *s){
+  struct data *d = malloc(sizeof(struct data) * 100);
+  d[0].size = 0;
+  d[0].chr = s[0]; //d[0] = {0, s[0]};
+  size_t c = 0;
+  
+  for (int i = 0; i < strlen(s); i++){
+    if (s[i] == d[c].chr) d[c].size++;
+    else{
+      c++;
+      d[c].size = 1;
+      d[c].chr = s[i];
+    }
+  }
+  d[c+1].chr = '\0';
+  return d;
+}
+
+int main(){
+  char s[] = "aaaabbc";
+  struct data *d = (struct data*)compress_data(s);
+  
+  for (int i = 0; d[i].chr != '\0'; i++){
+    printf("%d %c\n", d[i].size, d[i].chr);
+  }
+
+  // printf("%s\n", d);
+  // struct data d = {0, s[0]};
+  //
+  // for (int i = 0; i < strlen(s); i++){
+  //   if (s[i] == d.chr) d.size++;
+  //   else{
+  //     printf("%d %c\n", d.size, d.chr);
+  //     d.size = 1;
+  //     d.chr = s[i];
+  //   }
+  // }
+  // printf("%d %c\n", d.size, d.chr);
+
+  return 0;
+}
