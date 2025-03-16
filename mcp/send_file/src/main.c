f8336aa6a7b55a159d39a3cda66168c0b969c6d5
MaroB05 <s-marwan.hussien@zewailcity.edu.eg>
2025-03-16 21:11:02 +0200

    implemented cp with sendfile()

---
+++ mcp/send_file/src/main.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sendfile.h>
+
+#define size 4 * 1024
+int main(int argc, char* argv[]){
+
+  if (argc < 3){
+    fputs("usage: mcp file1 file2", stderr);
+    return -1;
+  }
+  
+  int src = open(argv[1], O_RDONLY);
+  if (src == -1){
+    printf("error opening %s", argv[1]);
+    perror("");
+    return -1;
+  }
+  
+  int dist = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+  if (dist == -1){
+    printf("error opening %s", argv[2]);
+    perror("");
+    return -1;
+  }
+
+  int file_size = lseek(src, 0, SEEK_END);
+  lseek(src, 0, SEEK_SET);
+  printf("file size: %d\n", file_size);
+  sendfile(dist, src, NULL, file_size);
+
+  return 0;
+}
